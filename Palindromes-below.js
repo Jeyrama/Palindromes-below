fc0d43f42a2d76b8b5bbc1a95fdfd8e78e125e1f
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-06-03 21:11:58 -0700

    Create Palindromes-below.js

---
+++ Palindromes-below.js
@@ -0,0 +1,16 @@
+/*
+The aim of this challenge is to modify the Fixnum class to give it the palindrome_below method. 
+This method returns all numbers from and including 1 up to 
+but not including itself that are palindromes for a given base.
+
+For example in base 2 (binary)
+  1 = "1"
+  2 = "10"
+  3 = "11"
+  4 = "100"
+
+Therefore 1 and 3 are palindromes in base two 
+and the method should return the following:
+  5..palindromeBelow(2)
+  => [1, 3]
+*/
